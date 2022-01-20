b173698fecf20bd125b271e0cd79a2d0e1aeb4d1
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2022-01-20 22:47:20 +0100

    began guitar content project

--- projects/notion-api-test/tasks.py
+++ projects/notion-api-test/tasks.py
@@ -29,11 +29,31 @@ def check_task(database_id):
             }
         }
     )
-
-    print("got database")
     if 'results' in query and query['results']:
         for page in query['results']:
             update_page.delay(page)
+
+@app.task
+def guitar_day(database_id):
+    # run this task each day and add a new page with guitar practice content such as suggested songs, scales, chords, riffs, techniques etc.
+    notion = Client(auth=os.environ["NOTION_API_TOKEN"])
+
+    # query database to ensure it's available
+    query = notion.databases.query(
+        **{
+            'database_id': database_id,
+            'filter': {
+
+            }
+        }
+    )
+
+    # create new page with guitar practice content
+
+    page = query['result']
+
+    # spawn new page ready for user to engage in
+    update_page.delay(page)
 
 @app.task#(queue='updater')
 def update_page(page):
